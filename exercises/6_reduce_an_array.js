789d426bd60314509768e410f5ade42f171e9351
dom <dbattistini@clearbridgemedia.com>
2019-05-20 14:29:34 -0400

    xompleted exercise 6

--- exercises/6_reduce_an_array.js
+++ exercises/6_reduce_an_array.js
@@ -6,7 +6,11 @@ let totalOfOddNumbers = numberList.reduce(function(accumulator, currentValue) {
   // Code this reducer function so that the result is the sum
   // of only the odd values (3, 7, 19, etc). Do not alter any other code.
   // PLACE YOUR CODE BELOW
+  if(currentValue%2===1){
+    accumulator = accumulator + currentValue
+  };
 
+  return accumulator;
   // PLACE YOUR CODE ABOVE
 }, 0)
 
